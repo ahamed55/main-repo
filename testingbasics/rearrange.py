c4afdf12778de47c38a3c95acc0ca19db8cd530f
ahamed <ibrahmed98@gmail.com>
2021-09-17 13:34:07 +0530

    updated test files

--- testingbasics/rearrange.py
+++ testingbasics/rearrange.py
@@ -2,6 +2,7 @@ import re
 from typing import Pattern
 
 def rearrange_name(name):
-    result = re.search(r"^(\w*) (\w*)",name)
+    result = re.search(r"^([\w]*),? ([\w, \.]*)",name)
+    if result == None:
+        return ""
     return f"{result[2]} {result[1]}"
-
--- testingbasics/rearrange_test.py
+++ testingbasics/rearrange_test.py
@@ -1,10 +1,21 @@
+from unittest.case import TestCase
 import rearrange
 import unittest
 
 class test(unittest.TestCase):
     def test_arrange(self):
         testcase = "ahamed ibrahim"
-        expected = "ibrahim ahame d"
+        expected = "ibrahim ahamed"
         self.assertEqual(rearrange.rearrange_name(testcase),expected)
-        
+
+    def test_emptystr(self):
+        TestCase = ""
+        expected = ""
+        self.assertEqual(rearrange.rearrange_name(TestCase),expected)
+    
+    def test_double_word(self):
+        testcase = "ahamed, ibrahim.A"
+        expected = "ibrahim.A ahamed"
+        self.assertEqual(rearrange.rearrange_name(testcase),expected)
+         
 unittest.main()
